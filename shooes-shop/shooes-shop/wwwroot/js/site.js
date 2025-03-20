062ba4852a37a4a9aa40df30d0b501ae7632dd87
Vasyl Stolyarchuk <Vasyl.Stolyarchuk@outlook.com>
2025-03-20 22:07:50 +0200

    add styles and images

---
+++ shooes-shop/shooes-shop/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿window.toggleNavbar = () => {
+    let nav = document.getElementById("navbarNav");
+    if (nav.classList.contains("show")) {
+        nav.classList.remove("show");
+    } else {
+        nav.classList.add("show");
+    }
+};
+
+window.toggleTheme = () => {
+    document.body.classList.toggle("dark-mode");
+};
